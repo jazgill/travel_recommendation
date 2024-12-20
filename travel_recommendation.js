4735934e36c8a1d7996f693cc689cc4f7f3599d0
jazgill <jazzgill585@gmail.com>
2024-12-20 13:35:26 -0800

    empty repository for travel recommendation. Added main page.

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -4,6 +4,13 @@ const searchButton = document.getElementById('btnSearch');
 const clearButton = document.getElementById('btnClear');
 const destinations = [];
 
+function bookNowFunction(){
+    console.log("We have clicked bookNow!!");
+    resetResults();
+}
+function resetResults(){
+    const input = document.getElementById("searchInput").value = "";
+}
 
 function searchDestination(){
     const input = document.getElementById("searchInput").value.toLowerCase();
@@ -11,15 +18,15 @@ function searchDestination(){
     resultView.innerHTML = '';
 
     fetch('travel.json')
-        .then(response => response.json())
+        .then((response) => response.json())
         .then(data => {
             // Work with JSON data here
             if(input !== ''){
-                if(input.includes('beach')){
+               if(input.includes('beach' || 'beaches')){
                     const beaches = data.beaches;
                     beaches.forEach(beach => {
-                        resultView.innerHTML +=`
-                        <div class="resultView">
+                        resultView.innerHTML +=
+                        `<div class="resultView">
                         <div class="imageView"> 
                         <img src="${beach.imageUrl}" alt="${beach.name}">
                         </div>
@@ -28,21 +35,55 @@ function searchDestination(){
                         <p>${beach.description}</p>
                         </div>
                         </div>`;
-                        console.log("beachImage: " + beach.imageUrl);
-                        console.log("beach Name: " + beach.name);
-                        console.log("beach description: " + beach.description);
                     });
                 }
+                else if(input.includes('temple' || 'temples')){
+                    const temples = data.temples;
+                    temples.forEach(temple => {
+                        resultView.innerHTML += 
+                        `<div class="resultView">
+                        <div class="imageView">
+                        <img src="${temple.imageUrl}" alt="${temple.name}">
+                        </div>
+                        <div class="textView">
+                        <h2>${temple.name}</h2>
+                        <p>${temple.description}</p>
+                        </div>
+                        </div>`
+                    })
+                }
+                else if(input.includes('japan' || 'countries')){
+                    const countries = data.countries;
+                    countries.forEach(country => {
+                        resultView.innerHTML +=
+                        `<div class="resultView">
+                        <div class="imageView">
+                        <img src="${country.imageUrl}" alt="${country.name}">
+                        </div>
+                        <div class="textView">
+                        <h2>${country.name}</h2>
+                        <p>${country.description}</p>
+                        </div>
+                        </div>`
+                    })
+                }
+                else {
+                    resultView.innerHTML = `<div class="resultView">
+                    <div class="textView">
+                    <h2>No Destination(s) found based on your search.  Please try again!!</h2>`
+                }
             }
-            
         })
         .catch(error => {
             console.error('Error:', error);
             console.log("An error occurred while fetching data.");
-            //resultView.innerHTML = 'An error occurred while fetching data.';
+            resultView.innerHTML = `<h1>An error occurred while fetching data.</h1>`;
         });
+        event.preventDefault();
 } 
 
-searchButton.addEventListener('click', searchDestination);
 
+bookNow.addEventListener('click', bookNowFunction);
+clearButton.addEventListener('click', resetResults);
+searchButton.addEventListener('click', searchDestination);
 
