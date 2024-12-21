f8ea4286dad88ca44040d571509808ec410225f0
jazgill <jazzgill585@gmail.com>
2024-12-21 12:07:45 -0800

    Implemented the functionality to search for beach or beaches destination, or temple or temples destination and also added funtionality to search based on country and country name.  Added timezone for each country searched.

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,12 +2,13 @@ const bookNowBtn = document.getElementById("bookNow");
 const result = document.getElementById("result");
 const searchButton = document.getElementById('btnSearch');
 const clearButton = document.getElementById('btnClear');
-const destinations = [];
+//const countries = [];
 
 function bookNowFunction(){
     console.log("We have clicked bookNow!!");
     resetResults();
 }
+
 function resetResults(){
     const input = document.getElementById("searchInput").value = "";
 }
@@ -32,7 +33,8 @@ function searchDestination(){
                         </div>
                         <div class="textView">
                         <h2>${beach.name}</h2>
-                        <p>${beach.description}</p>
+                        <p>${beach.description}</p><br>
+                        <button id="visit">Visit</button>
                         </div>
                         </div>`;
                     });
@@ -47,24 +49,98 @@ function searchDestination(){
                         </div>
                         <div class="textView">
                         <h2>${temple.name}</h2>
-                        <p>${temple.description}</p>
+                        <p>${temple.description}</p><br>
+                        <button id="visit">Visit</button>
                         </div>
-                        </div>`
+                        </div>`;
                     })
                 }
-                else if(input.includes('japan' || 'countries')){
+                else if(input.includes('country' || 'countries')){
                     const countries = data.countries;
                     countries.forEach(country => {
-                        resultView.innerHTML +=
+                        country.cities.forEach(city => {
+                            resultView.innerHTML += 
+                            `<div class="resultView">
+                            <div class="imageView">
+                                <img src="${city.imageUrl}" alt="${city.name}">
+                            </div>
+                            <div class="textView">
+                            <h2>${country.name}</h2>
+                            <h4>${city.name}</h4>
+                            <p>${city.description}</p><br>
+                            <button id="visit">Visit</button>
+                            </div>
+                            </div>`;
+        /*const options = { timeZone: 'America/New_York', hour12: true, hour: 'numeric', 
+        minute: 'numeric', second: 'numeric' };
+        const newYorkTime = new Date().toLocaleTimeString('en-US', options);
+        console.log("Current time in New York:", newYorkTime);*/
+                        })
+                    })
+                }
+                else if(input.includes('australia')){
+                    const countryName = data.countries.find(item => item.name.toLowerCase() === input);
+                    const options = { timeZone: 'Australia/Sydney', hour12: true, hour: 'numeric', 
+                    minute: 'numeric', second: 'numeric' };
+                    const ausTime = new Date().toLocaleTimeString('en-US', options);
+                    countryName.cities.forEach(city => {
+                        console.log("City: " + city.name);
+                        resultView.innerHTML += 
                         `<div class="resultView">
                         <div class="imageView">
-                        <img src="${country.imageUrl}" alt="${country.name}">
+                        <img src="${city.imageUrl}" alt="${city.name}">
                         </div>
                         <div class="textView">
-                        <h2>${country.name}</h2>
-                        <p>${country.description}</p>
+                        <h2>${countryName.name}</h2>
+                        <h4>${city.name}</h4>
+                        <p>${city.description}</p><br>
+                        <h5>Current Time:${ausTime}</h5><br>
+                        <button id="visit">Visit</button>
                         </div>
-                        </div>`
+                        </div>`;
+                    })
+                }
+                else if(input.includes('japan')){
+                    const countryName = data.countries.find(item => item.name.toLowerCase() === input);
+                    const options = { timeZone: 'Asia/Tokyo', hour12: true, hour: 'numeric', 
+                    minute: 'numeric', second: 'numeric' };
+                    const japTime = new Date().toLocaleTimeString('en-US', options);
+                    countryName.cities.forEach(city => {
+                        resultView.innerHTML += 
+                        `<div class="resultView">
+                        <div class="imageView">
+                        <img src="${city.imageUrl}" alt="${city.name}">
+                        </div>
+                        <div class="textView">
+                        <h2>${countryName.name}</h2>
+                        <h4>${city.name}</h4>
+                        <p>${city.description}</p><br>
+                        <h5>Current Time:${japTime}</h5><br>
+                        <button id="visit">Visit</button>
+                        </div>
+                        </div>`;
+                    })
+                }
+                else if(input.includes('brazil')){
+                    const countryName = data.countries.find(item => item.name.toLowerCase() === input);
+                    const options = { timeZone: 'America/Rio_Branco'    , hour12: true, hour: 'numeric', 
+                    minute: 'numeric', second: 'numeric' };
+                    const brazilTime = new Date().toLocaleTimeString('en-US', options);
+                    countryName.cities.forEach(city => {
+                        console.log("City: " + city.name);
+                        resultView.innerHTML += 
+                        `<div class="resultView">
+                        <div class="imageView">
+                        <img src="${city.imageUrl}" alt="${city.name}">
+                        </div>
+                        <div class="textView">
+                        <h2>${countryName.name}</h2>
+                        <h4>${city.name}</h4>
+                        <p>${city.description}</p><br>
+                        <h5>Current Time:${brazilTime}</h5><br>
+                        <button id="visit">Visit</button>
+                        </div>
+                        </div>`;
                     })
                 }
                 else {
